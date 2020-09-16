571b071f5320e8b8eeadc83589954506458cde6e
Hoon Seo <seohoon@mymail.mines.edu>
2020-09-16 15:51:15 -0600

    implement dicionaries merging recursively

--- utilsforminds/containers.py
+++ utilsforminds/containers.py
@@ -7,7 +7,7 @@ def get_items_from_list_conditionally(list_, condition_function, whether_deepcop
     --------
     test_list = [{'a':1, 'b':2}, {'a':3, 'b':2}, {'a':2, 'b':4}]\n
     print(get_items_from_list_conditionally(test_list, lambda x: True if x['a'] >= 2 else False))
-        == [{'a': 3, 'b': 2}, {'a': 2, 'b': 4}]
+        [{'a': 3, 'b': 2}, {'a': 2, 'b': 4}]
     """
 
     assert(type(list_) == type([]))
@@ -17,13 +17,40 @@ def get_items_from_list_conditionally(list_, condition_function, whether_deepcop
             result_collection.append(deepcopy(item)) if whether_deepcopy else result_collection.append(item)
     return result_collection
 
-def merge_dictionaries(list_of_dicts : list, use_last_when_overlapped = True):
+def merge_two_dicts_leaves_rec(dict_1, dict_2):
+    """ Merge two dictionaries recursively.
+    Examples
+    --------
+    test_dict_1 = {"n": ["a", "b", "c"], "m": {"hi": 4}, 1: ["h", "i"], "dup": {"ace": {1: 2, 3: 4}}, "none": None}
+    test_dict_2 = {"n": ["a", "d"], "m": {"hi" : 2, "hello": {3: 2}}, 2: 4, "dup": {"ace": {1: {4: 5, 6: 7}, 3: None}}, "none": 4}
+    print(merge_two_dicts_leaves_rec(test_dict_1, test_dict_2))
+        {'n': ['a', 'd'], 'm': {'hi': 2, 'hello': {3: 2}}, 2: 4, 'dup': {'ace': {1: {4: 5, 6: 7}, 3: None}}, 'none': 4, 1: ['h', 'i']}
+    """
+
+    if dict_1 is None:
+        return dict_2
+    if dict_2 is None:
+        return dict_1
+    assert(isinstance(dict_1, dict) and isinstance(dict_2, dict))
+    merged_dict = {}
+    for dict_2_key in dict_2.keys():
+        if isinstance(dict_2[dict_2_key], dict) and dict_2_key in dict_1.keys() and isinstance(dict_1[dict_2_key], dict):
+            merged_dict[dict_2_key] = merge_two_dicts_leaves_rec(dict_1[dict_2_key], dict_2[dict_2_key])
+        else:
+            merged_dict[dict_2_key] = dict_2[dict_2_key]
+    for dict_1_key in dict_1.keys():
+        if dict_1_key not in merged_dict.keys():
+            merged_dict[dict_1_key] = dict_1[dict_1_key]
+    return merged_dict
+            
+
+def merge_dictionaries(list_of_dicts : list, use_last_when_overlapped = True, recursive_overwritting = True):
     """ Merge dictionaries with 'update' method.
-        Always deepcopy because of nature of dict.update method.
+        Always deepcopy because of nature of dict.update method. -> Changed to shallowcopy 200910.
 
     Examples
     --------
-    test_list = [{'a':1, 'b':2}, {'a':3, 'b':2}, {'a':2, 'b':4}]\n
+    test_list = [{'a':1, 'b':2, 'c':9}, {'a':3, 'b':2}, {'a':2, 'b':4}]\n
     merge_dictionaries(test_list, use_last_when_overlapped = True)\n
         == {'a': 2, 'b': 4, 'c': 9}
     merge_dictionaries(test_list, use_last_when_overlapped = False)\n
@@ -36,9 +63,10 @@ def merge_dictionaries(list_of_dicts : list, use_last_when_overlapped = True):
     else:
         range_to_iter = range(len(list_of_dicts) -1, -1, -1)
     for dict_idx in range_to_iter:
-        if list_of_dicts[dict_idx] is not None:
-            assert(type(list_of_dicts[dict_idx]) == type({}))
-            merged_dict.update(list_of_dicts[dict_idx])
+        # if list_of_dicts[dict_idx] is not None:
+        #     assert(type(list_of_dicts[dict_idx]) == type({}))
+        #     merged_dict.update(list_of_dicts[dict_idx])
+        merged_dict = merge_two_dicts_leaves_rec(merged_dict, list_of_dicts[dict_idx])
     return merged_dict
 
 def merge_lists(list_of_lists : list, use_last_when_overlapped = True):
@@ -81,5 +109,12 @@ def copy_dict_and_delete_element(dict_to_copy, list_of_keys_to_delete):
     return result
 
 if __name__ == "__main__":
-    test_dict = {"n": ["a", "b", "c"], "m": ["d", "e", "f", "g"], 1: ["h", "i"]}
-    print(copy_dict_and_delete_element(dict_to_copy = test_dict, list_of_keys_to_delete = ["n", 1]))
+    # test_dict_1 = {"n": ["a", "b", "c"], "m": {"hi": 4}, 1: ["h", "i"], "dup": {"ace": {1: 2, 3: 4}}, "none": None}
+    # test_dict_2 = {"n": ["a", "d"], "m": {"hi" : 2, "hello": {3: 2}}, 2: 4, "dup": {"ace": {1: {4: 5, 6: 7}, 3: None}}, "none": 4}
+    # print(merge_two_dicts_leaves_rec(test_dict_1, test_dict_2))
+
+    # print(copy_dict_and_delete_element(dict_to_copy = test_dict, list_of_keys_to_delete = ["n", 1]))
+
+    test_list = [{'a':1, 'b':2, 'c':9}, {'a':3, 'b':2}, {'a':2, 'b':4}]
+    print(merge_dictionaries(test_list, use_last_when_overlapped = True))
+    print(merge_dictionaries(test_list, use_last_when_overlapped = False))
