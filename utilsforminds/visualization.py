b5ea9da5688bc32c6029b9c06d31afe9fbb88d68
Hoon Seo <seohoon@mymail.mines.edu>
2020-12-06 11:28:47 -0700

    handle nan/inf case in plot_bar_charts function.

--- utilsforminds/visualization.py
+++ utilsforminds/visualization.py
@@ -647,6 +647,13 @@ def plot_bar_charts(path_to_save : str, name_numbers : dict, xlabels : list = No
             y-axis cut range.
     """
 
+    ## Input sanity check
+    for check_target in [name_numbers, name_errors]:
+        for numbers_list in check_target.values():
+            if any(pd.isna(numbers_list)): ## At least one element is nan/inf.
+                print(f"WARNING: This function plot_bar_charts encounters invalid numbers such as nan/inf, so will be passed and do nothing.")
+                return None
+
     names = list(name_numbers.keys())
     ## Set kwargs parameters
     plt_bars_kwargs_dict = {}
